9b19a9a14f3195d8ac07e2f4d72e11302dcff153
PatsyMorales <pat.sy@live.com>
2014-03-20 11:48:29 -0600

    Agregado las clases de la conexion a la base de datos

---
+++ Proyecto_interfaz/Proyecto_interfaz/BaseDeDatos.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Odbc;
+using System.Data;
+using System.Windows.Forms;
+
+
+namespace Proyecto_interfaz
+{
+    class BaseDeDatos
+    {
+       public Conexion C = new Conexion();
+        public OdbcDataReader cnLeerConsulta;
+
+        public void Abrir()
+        {
+            C.ConectorC("dsn=sysmed");  //nombre del DSN. Hay que configurarlo (inicio,odbc,dsn de sistema,configurar)
+            C.AbrirConexion();
+        }
+
+        public void actualizar(String consulta)
+        {
+            OdbcCommand cnSentencia = new OdbcCommand(consulta, C.cnConexion);
+            cnSentencia.CommandType = CommandType.StoredProcedure;
+            cnSentencia.ExecuteNonQuery();
+            C.CerrarConexion(C.cnConexion);
+        }
+
+        public void leer(String consulta)
+        {
+            // toma la consulta y abre conexion con la clase conexion
+            OdbcCommand cnConsulta = new OdbcCommand(consulta, C.cnConexion);
+            cnConsulta.CommandType = CommandType.StoredProcedure;
+            cnLeerConsulta = cnConsulta.ExecuteReader();
+        }
+
+        public void cerrar()
+        {
+            //leer mandamos la variable de conexion para cerrar
+            C.CerrarConexion(C.cnConexion);
+        }
+
+
+    }
+}
---
+++ Proyecto_interfaz/Proyecto_interfaz/Conexion.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Data.Odbc;
+using System.Windows.Forms;
+using System.Data;
+
+namespace Proyecto_interfaz
+{
+    class Conexion
+    {
+        public OdbcConnection cnConexion = new OdbcConnection();
+        public String DSN;
+
+        public void ConectorC(String CadenaDSN)
+        {
+            this.DSN = CadenaDSN;
+        }
+
+        public void AbrirConexion()
+        {
+            try
+            {
+                cnConexion.ConnectionString = this.DSN; //toma el valor del DNS
+                cnConexion.Open();
+
+            }
+            catch (OdbcException ex)
+            {
+                MessageBox.Show("Error " + Convert.ToString(ex));
+            }
+        }
+
+        public void CerrarConexion(OdbcConnection cnConexion)
+        {
+            if (cnConexion.State.Equals(ConnectionState.Open)) //si el estado de la conexion esta abierta, cierra la conexion
+            {
+                cnConexion.Close();
+            }
+        }
+
+       
+    }
+}
