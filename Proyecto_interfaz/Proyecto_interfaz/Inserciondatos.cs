93de2b86fc3a3a4dec3dd823de25eb05557cebc1
jlcorona <jlcorona2889@gmail.com>
2014-03-20 12:14:16 -0600

    Insercion de datos
    Para el llenado del data grid de citas

---
+++ Proyecto_interfaz/Proyecto_interfaz/Inserciondatos.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Proyecto_interfaz
+{
+    class Inserciondatos
+    {
+        public string Item { get; set; }
+        public string Value { get; set; }
+    }
+}
